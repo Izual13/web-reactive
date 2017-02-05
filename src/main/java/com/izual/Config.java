d4d2edb1d4a90e5f94b8fdbe328012703eb85002
Izual <mtsoy1990@gmail.com>
2017-02-05 08:39:57 +0300

    update spring boot version

--- src/main/java/com/izual/Config.java
+++ src/main/java/com/izual/Config.java
@@ -2,14 +2,13 @@ package com.izual;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.client.reactive.WebClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class Config {
 
     @Bean
     WebClient webClient() {
-        return new WebClient(new ReactorClientHttpConnector());
+        return WebClient.create();
     }
 }
--- src/main/java/com/izual/controller/ReactiveController.java
+++ src/main/java/com/izual/controller/ReactiveController.java
@@ -4,7 +4,7 @@ package com.izual.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.reactive.WebClient;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 @RestController
